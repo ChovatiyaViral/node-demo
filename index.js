b6e3758c2639d731f78cef35a6a88728dd2676a1
ChovatiyaViral <vchovatiya992@gmail.com>
2022-02-20 14:37:14 +0530

    - install json and envdot package

---
+++ index.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+
+mongoose.connect("mongodb://localhost/mongotube", { useNewUrlParser: true });
+
+mongoose.connection
+    .once('open', () => console.log("connected ...."))
+    .on('error', (error) => console.log(error))
